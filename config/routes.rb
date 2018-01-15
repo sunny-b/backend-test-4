8f6b262c8f3cd7b0969092e3f45ae5e524b1743d
Sunny <sjbeatteay@gmail.com>
2018-01-14 23:34:57 -0500

    create forward_call and voicemail paths and methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post '/calls/menu', to: 'calls#main_menu', as: :menu
-  post '/calls/menu_selection', as: :menu_selection
-  post '/calls/voicemail', as: :voicemail
+  post '/ivr', to: 'ivr#main_menu', as: :menu
+  post '/ivr/menu_selection', as: :menu_selection
+  post '/ivr/voicemail', as: :voicemail
 end
